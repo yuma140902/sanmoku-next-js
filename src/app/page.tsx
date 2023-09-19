8818559b524cd36e45485f4f5edf5802fc8a9722
yuma14 <yuma140902@gmail.com>
2023-09-20 00:14:13 +0900

    石が打てるようになった

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,66 @@
-import Image from 'next/image'
+"use client"
+import { useState } from "react"
+
+type Player = 'white' | 'black';
+type CellState = 'empty' | 'white' | 'black';
+
+type CellProps = {
+  state: CellState
+  handleClick: () => void
+}
+
+function Cell(props: CellProps) {
+  return (
+    <button onClick={props.handleClick}>
+      {props.state == 'empty' ? 'empty ' : props.state == 'white' ? '○' : '●'}
+    </button>
+  )
+}
+
+function Board() {
+  const [player, setPlayer] = useState<Player>('white');
+  const [cells, setCells] = useState<CellState[]>(Array(9).fill('empty'));
+
+  function handleClick(index: number) {
+    if (cells[index] === 'empty') {
+      if (player === 'white') {
+        setCells(cells.map((old, i) => i == index ? 'white' : old));
+        setPlayer('black')
+      }
+      else if (player === 'black') {
+        setCells(cells.map((old, i) => i == index ? 'black' : old));
+        setPlayer('white')
+      }
+    }
+  }
+
+  function createCell(index: number) {
+    return <Cell state={cells[index]} handleClick={() => handleClick(index)} />
+  }
+
+  return (
+    <div>
+      <div>
+        {createCell(0)}
+        {createCell(1)}
+        {createCell(2)}
+      </div>
+      <div>
+        {createCell(3)}
+        {createCell(4)}
+        {createCell(5)}
+      </div>
+      <div>
+        {createCell(6)}
+        {createCell(7)}
+        {createCell(8)}
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-    <div>empty</div>
+    <Board />
   )
 }
